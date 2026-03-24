127ed72b4c0ba0f6cd82964b0ada9fd39cf64320
Zhivko2005 <zhivko.hadzhiev05@gmail.com>
2026-03-24 19:45:15 +0200

    Implemented image recognition logic

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -1,24 +1,33 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()
-
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 class TravelService:
     @staticmethod
-    def generate_plan(destination: str, days: int):
-        """Изпраща промпт към Gemini и връща генерирания план."""
-
+    def generate_plan(destination: str, days: int, rating: int, image_bytes: bytes = None):
         model_id = "gemini-2.5-flash"
 
         prompt = (
-            f"Ти си професионален ескурзовод. Направи вълнуващ и кратък "
-            f"туристически план за {days} дни в {destination}. Използвай булети и емотикони."
+            f"Ти си професионален туристически eксурзовод. Направи план за {days} дни в {destination}. "
+        f"ВАЖНО: Препоръчай поне 3 хотела с категория {rating} звезди в тази дестинация. "
+        f"Опиши защо са подходящи. Раздели плана по дни и използвай емотикони."
         )
+        contents = []
 
+        if image_bytes:
+            contents.append(types.Part.from_bytes(data=image_bytes, mime_type="image/jpg"))
+            prompt      +=("Анализирай внимателно тази снимка. Определи точното местоположение (град и държава). "
+                f"След като го идентифицираш, направи туристически план за {days} дни в този град. "
+                f"Включи хотели с {rating} звезди. Отговори на български.")
+        else:
+            prompt  += f" Дестинацията е: {destination}."
+
+        contents.append(prompt)
         try:
             response = client.models.generate_content(
                 model=model_id,
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, UploadFile, File, Form, Query
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from app.services.gemini_service import TravelService
 
-app = FastAPI(title="AI Travel Guide API")
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 async def read_index():
     return FileResponse('static/index.html')
 
-@app.get("/generate")
-def generate(dest: str, days: int = 2):
-    plan = TravelService.generate_plan(dest, days)
+
+@app.post("/generate")
+async def generate(
+        dest: str = Form(None),
+        days: int = Form(3),
+        rating: int = Form(3),
+        image: UploadFile = File(None)
+):
+    image_data = None
+    if image:
+        image_data = await image.read()
+
+    plan = TravelService.generate_plan(
+        destination=dest,
+        days=days,
+        rating=rating,
+        image_bytes=image_data
+    )
+
     return {"plan": plan}

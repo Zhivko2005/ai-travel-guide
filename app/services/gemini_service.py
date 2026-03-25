7bf2b85f4d604b51e0e0d2cfd52e7d84e8aa27c7
Zhivko2005 <zhivko.hadzhiev05@gmail.com>
2026-03-25 14:13:46 +0200

    New prompts and temperature settings to prevent AI halucinations

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -12,26 +12,36 @@ class TravelService:
     def generate_plan(destination: str, days: int, rating: int, image_bytes: bytes = None):
         model_id = "gemini-2.5-flash"
 
-        prompt = (
-            f"Ти си професионален туристически eксурзовод. Направи план за {days} дни в {destination}. "
-        f"ВАЖНО: Препоръчай поне 3 хотела с категория {rating} звезди в тази дестинация. "
-        f"Опиши защо са подходящи. Раздели плана по дни и използвай емотикони."
-        )
         contents = []
 
         if image_bytes:
-            contents.append(types.Part.from_bytes(data=image_bytes, mime_type="image/jpg"))
-            prompt      +=("Анализирай внимателно тази снимка. Определи точното местоположение (град и държава). "
-                f"След като го идентифицираш, направи туристически план за {days} дни в този град. "
-                f"Включи хотели с {rating} звезди. Отговори на български.")
-        else:
-            prompt  += f" Дестинацията е: {destination}."
+            contents.append(types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg"))
 
-        contents.append(prompt)
+            prompt_text = (
+                f"Ти си професионален туристически eкскурзовод."
+                f"Анализирай какво виждаш на снимката: архитектура, надписи, регистрационни номера, специфични обекти, без да ги поясняваш в отговора си"
+                f"Въз основа на тези доказателства, идентифицирай града и държавата, като стриктно по шаблон : Въз основа на снимковия материал идентифицирах че града е /.../"
+                f"СЛЕД разпознаването, препоръчай поне 3 хотела с категория {rating} звезди в тази дестинация. "
+                f"Направи туристически план за {days} дни в тази дестинация"
+                f"Използвай емотикони и НЕ използвай bold-ване на текста"
+            )
+        else:
+            prompt_text = (
+            f"Ти си професионален туристически eкскурзовод. Направи план за {days} дни в {destination}. "
+            f"ВАЖНО: Препоръчай поне 3 хотела с категория {rating} звезди в тази дестинация. "
+            f"Опиши защо са подходящи. Раздели плана по дни и използвай емотикони и НЕ използвай bold-ване на текста."
+            )
+        contents.append(prompt_text)
+
         try:
             response = client.models.generate_content(
                 model=model_id,
-                contents=prompt
+                contents=contents,
+                config=types.GenerateContentConfig(
+                    temperature=0.3,
+                    top_p=0.95,
+                    max_output_tokens=5000
+                )
             )
             return response.text
         except Exception as e:

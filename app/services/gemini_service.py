b8769a4f0b56579eaf07275bdab20e0d0bb43f9e
Zhivko2005 <zhivko.hadzhiev05@gmail.com>
2026-03-26 12:30:49 +0200

    Added comments in the backend

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -3,17 +3,23 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+#Зареждане на GEMINI_API_KEY
 load_dotenv()
+#Инициализация на клиента
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 class TravelService:
     @staticmethod
     def generate_plan(destination: str, days: int, rating: int, image_bytes: bytes = None):
+
+        #Идентификатор на модела
         model_id = "gemini-2.5-flash"
 
+        #Списък с частите, които изпращаме на ИИ
         contents = []
 
+        #Логика в случай в който използваме снимка
         if image_bytes:
             contents.append(types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg"))
 
@@ -25,26 +31,31 @@ class TravelService:
                 f"Направи туристически план за {days} дни в тази дестинация"
                 f"Използвай емотикони и НЕ използвай bold-ване на текста"
             )
+        #Логика в случай в който използваме информацията от текстбокса
         else:
             prompt_text = (
             f"Ти си професионален туристически eкскурзовод. Направи план за {days} дни в {destination}. "
             f"ВАЖНО: Препоръчай поне 3 хотела с категория {rating} звезди в тази дестинация. "
             f"Опиши защо са подходящи. Раздели плана по дни и използвай емотикони и НЕ използвай bold-ване на текста."
             )
+        #Добавяне на промпта към компонентите
         contents.append(prompt_text)
 
         try:
+            #Изпращане на заявката към API-то
             response = client.models.generate_content(
                 model=model_id,
                 contents=contents,
+                #Задаване на настройки
                 config=types.GenerateContentConfig(
-                    temperature=0.3,
-                    top_p=0.95,
-                    max_output_tokens=5000
+                    temperature=0.3, #По-ниска температура - баланс между логично мислене и креативност
+                    top_p=0.95, #Контрол на разнообразието на думите
+                    max_output_tokens=5000 #Лимит на дължината на отговора
                 )
             )
             return response.text
         except Exception as e:
+            #HTTP error 429 - Too many requests - За да не гърми приложението при достигнат лимит при заявки
             if "429" in str(e):
                 return "Грешка: Лимитът на безплатни заявки е достигнат. Моля, изчакайте 1 минута."
             return f"Възникна грешка при връзка с ИИ: {str(e)}"
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, UploadFile, File, Form, Query
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from app.services.gemini_service import TravelService
 
+#Инициализиране основното приложение на FastApi
 app = FastAPI()
 
+# Свръзваме папката статик с URL за зареждане на frontend-a
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -15,20 +17,24 @@ async def read_index():
 
 @app.post("/generate")
 async def generate(
+        #Зареждане на данни от HTML с Form(None)
         dest: str = Form(None),
         days: int = Form(3),
         rating: int = Form(3),
         image: UploadFile = File(None)
 ):
+    #Провека дали потребителя е качил изображение
     image_data = None
     if image:
+        #превръщане в байтове
         image_data = await image.read()
 
+    #Пращане на данните в TravelService
     plan = TravelService.generate_plan(
         destination=dest,
         days=days,
         rating=rating,
         image_bytes=image_data
     )
-
+    #Връщаме отговор в JSON формат към форнтенда
     return {"plan": plan}
